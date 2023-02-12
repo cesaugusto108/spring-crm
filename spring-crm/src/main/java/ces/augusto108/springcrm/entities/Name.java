71ea0623ec2c777daff927d07d68fde2c1c2a2f4
César Augusto <ces.augusto108@gmail.com>
2023-02-12 11:16:29 -0300

    Name entity class update

--- spring-crm/src/main/java/ces/augusto108/springcrm/entities/Name.java
+++ spring-crm/src/main/java/ces/augusto108/springcrm/entities/Name.java
@@ -3,13 +3,17 @@ package ces.augusto108.springcrm.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
 @Setter
 public class Name {
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     @Override
